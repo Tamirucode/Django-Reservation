d98e5cb423fb69faa29cb9b6bfb8236f6e078973
Tamirucode <tam.egu76@gmail.com>
2023-07-09 18:32:55 +0000

    initial commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bookmytable.wsgi
+web: gunicorn bookmytable.wsgi:application
--- bookmytable/settings.py
+++ bookmytable/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["tablereservation2023.herokuapp.com", "localhost" ]
+ALLOWED_HOSTS = ["tablereservation2023.herokuapp.com", "localhost"]
+
 
 
 # Application definition
@@ -82,12 +83,12 @@ WSGI_APPLICATION = 'bookmytable.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
+# DATABASES = {
+# 'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
