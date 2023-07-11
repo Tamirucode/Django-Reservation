687b7db0843fe41d71057d8f12399d449c5ee5e1
Tamirucode <tam.egu76@gmail.com>
2023-07-11 15:26:05 +0000

    deployment commit

--- bookmytable/settings.py
+++ bookmytable/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tablereservation2023-ee6a80ce91cb.herokuapp.com']
+ALLOWED_HOSTS = ['tablereservation2023-ee6a80ce91cb.herokuapp.com', 'localhost']
+
 
 
 
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Table(models.Model):
+    
+    TABLE_CATEGORY = [
+
+        ('B', 'BAR'),
+        ('D', 'DINING'),
+    ]
+    number = models.IntegerField(default=0)
+    category = models.CharField(max_length=1, choices=TABLE_CATEGORY)
+    seats = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f'Table{self.number}.inside {self.category} for {self.seats}  people'   
+    
+
+class Booking(models.Model):  
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    
+    time = models.TimeField()
+    date = models.DateField()
+    
+    def __str__(self):
+        return f'{self.user} booked {self.table}  Date {self.date} Time {self.time}'
+
+
+
